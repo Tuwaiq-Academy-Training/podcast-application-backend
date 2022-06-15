d1e7c71890c872d3aeeb171ad3c01a70418ec8f2
ranaqaboos <106505615+ranaqaboos@users.noreply.github.com>
2022-06-15 16:17:30 +0300

    first v

--- app.js
+++ app.js
@@ -1,1 +1,61 @@
-console.log("hello")
+const mongoose = require('mongoose');
+const express =require('express');
+const app = express();
+app.use(express.json());
+
+mongoose.connect('mongodb://localhost:27017/podcastApp');
+
+
+
+
+//Schema 1
+
+const userSchema =mongoose.Schema({
+    name: { type:String,
+        required:true
+    },
+  username:String,
+  gander:String,
+      email:String
+    })
+    const users= mongoose.model('user',userSchema);
+
+  //Schema 3
+    const commentSchema =mongoose.Schema({
+        descirption:String,
+        userID:{
+            type:mongoose.ObjectId,
+            ref:'user',
+           required:true
+          },
+          podcastID:{
+              type:mongoose.ObjectId,
+              ref:'podcast',
+             required:true
+            }
+        })
+  const comments= mongoose.model('comment',commentSchema);
+//Schema 2
+  const podcastSchema =mongoose.Schema({
+      name_podcast: String,
+      userID:{
+          type:mongoose.ObjectId,
+          ref:'user',
+          required:true
+        },
+        comments:{
+            type: [mongoose.ObjectId],
+            ref:"comment"
+        },
+    tags:String
+  })
+const podcasts= mongoose.model('podcast',podcastSchema);
+
+//insert
+app.post('/podcast/create/comment', (req,res)=>{
+    new comments((req.body)).save().then(() => res.json({"mag":"comment created"}));
+})
+//find
+app.get('/comment', (req,res)=>{  comments.find({}).populate('userId').then((data)=>{ res.json(data); })})
+
+ app.listen(3000,()=>{console.log("express started !")})
